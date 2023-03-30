eb5ce74786acd80cd3b227e2dc13c71e48973fe7
codarsh123 <99749486+codarsh123@users.noreply.github.com>
2023-03-30 17:36:25 +0530

    Add files via upload

---
+++ Week 6/soap assignment 21-03/target/generated-sources/jaxb/io/spring/guides/gs_producing_web_service/ObjectFactory.java
@@ -0,0 +1,47 @@
+package io.spring.guides.gs_producing_web_service;
+
+
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+
+	
+
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: io.spring.guides.gs_producing_web_service
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetEmployeeRequest }
+     * 
+     */
+    public GetEmployeeRequest createGetEmployeeRequest() {
+        return new GetEmployeeRequest();
+    }
+
+    /**
+     * Create an instance of {@link GetEmployeeResponse }
+     * 
+     */
+    public GetEmployeeResponse createGetEmployeeResponse() {
+        return new GetEmployeeResponse();
+    }
+
+    /**
+     * Create an instance of {@link Country }
+     * 
+     */
+    public Employee createEmployee() {
+        return new Employee();
+    }
+
+	
+	
+
+}
